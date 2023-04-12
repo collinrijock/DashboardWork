b202bfb17811ed7e1987bc777da720f1054b06e4
sdesaiLULA <sandeep@lula.is>
2023-04-11 22:56:37 -0400

    updated to use interpolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,13 @@ export default function Home() {
     const [logoutUrl, setLogoutUrl] = useState<string>()
     const [error, setError] = useState<any>()
     const app = initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+        apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_APIKEY}`,
+        authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}`,
+        projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
+        storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`,
+        messagingSenderId: `${process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}`,
+        appId: `${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}`,
+        measurementId: `${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`,
     });
     const provider = new GoogleAuthProvider();
     const auth = getAuth(app);
