0acaf57805e0343064b2897395d60820d353ef84
Stephen Schroeder <stephen.schroeder@lula.is>
2023-10-31 17:42:28 -0700

    fix form fields

--- pages/marketplace/index.tsx
+++ pages/marketplace/index.tsx
@@ -122,12 +122,55 @@ export default function AddAccount() {
         }        
     });
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
+    const { name, value } = event.target;
+
+    setFormData((prevData) => {
+        // Split the name by dot to access nested properties
+        const [category, property, subProperty, subSubProperty] = name.split('.');
+
+        if (subSubProperty) {
+            return {
+                ...prevData,
+                [category]: {
+                    ...prevData[category],
+                    [property]: {
+                        ...prevData[category][property],
+                        [subProperty]: {
+                            ...prevData[category][property][subProperty],
+                            [subSubProperty]: value,
+                        },
+                    },
+                },
+            };
+        }  
+        else if (subProperty) {
+            return {
+                ...prevData,
+                [category]: {
+                    ...prevData[category],
+                    [property]: {
+                        ...prevData[category][property],
+                        [subProperty]: value,
+                    },
+                },
+            };
+        } else if (property) {
+            return {
+                ...prevData,
+                [category]: {
+                    ...prevData[category],
+                    [property]: value,
+                },
+            };
+        } else {
+            return {
+                ...prevData,
+                [category]: value,
+            };
+        }
+        });
     };
+         
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
@@ -153,77 +196,278 @@ export default function AddAccount() {
 
         return(
     <form onSubmit={handleSubmit}>
-
-        <div>
-                <div>
-                    <h2>Add Account</h2>
-                    <input type={"text"} name="BusinessLegalName" value={formData.BusinessLegalName} onChange = {handleChange} placeholder={"Business Legal Name"} /><br/>                
-                    <input type={"text"} name="BusinessAddressLine1" value={formData.BusinessAddressLine1} onChange={handleChange} placeholder={"Line 1"} />
-                    <input type={"text"} name="BusinessAddressLine2" value={formData.BusinessAddressLine2} onChange={handleChange} placeholder={"Line 2"} /><br/>
-                    <input type={"text"} name="BusinessCity" value={formData.BusinessCity} onChange={handleChange} placeholder={"Business City"} />
-                    <input type={"text"} name="BusinessState" value={formData.BusinessState} onChange={handleChange} placeholder={"Business State"} />
-                    <input type={"text"} name="BusinessZipCode" value={formData.BusinessZipCode} onChange={handleChange} placeholder={"Business Zip"} />                
-                </div>
-                <div>
-                    <br/>
-                    <h2>Add Auto Liability</h2>
-                
-                    <input type={"text"} name="formData.autoLiability.policyNumber" value={formData.autoLiability.policyNumber} onChange={handleChange} placeholder={"Policy Number"} />
-                    <input type={"text"} name="autoLiability.startDate" placeholder={"Effective Date"} />
-                    <input type={"text"} name="autoLiability.endDate" placeholder={"Expiration Date"} />
-                    <select name="autoLiability.insurerId">
-                        {insurers.map((insurer) => {
-                            return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
-                        })}
-                    </select>
-                    <input type={"text"} name="autoLiability.coverageData.combinedSingleLimit" placeholder={"Combined Single Limit"} />
-                </div>
-                <div>
-                    <br/>
-                    <h2>Add Cargo</h2>
-                    
-                    <input type={"text"} name="policyNumber" placeholder={"Policy Number"} />
-                    <input type={"text"} name="startDate" placeholder={"Effective Date"} />
-                    <input type={"text"} name="endDate" placeholder={"Expiration Date"} />
-                    <select name="insurerId">
-                        {insurers.map((insurer) => {
-                            return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
-                        })}
-                    </select>
-                    <input type={"text"} name="limit" placeholder={"Cargo Limit"} />
-                    <input type={"text"} name="deductible" placeholder={"Cargo Deductible"} />
-                    <input type={"text"} name="Trailer Interchange Limit" placeholder={"Trailer Interchange Limit"} />
-                    <input type={"text"} name="reeferDeductible" placeholder={"Reefer Deductible"} />
-                    
-                </div>
-                <div>
-                    <br/>
-                    <h2>Add Physical Damage</h2>
-    
-                    <input type={"text"} name="policyNumber" placeholder={"Policy Number"} />
-                    <input type={"text"} name="startDate" placeholder={"Effective Date"} />
-                    <input type={"text"} name="endDate" placeholder={"Expiration Date"} />
-                    <select name="insurerId">
-                        {insurers.map((insurer) => {
-                            return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
-                        })}
-                    </select>
-                    <input type={"text"} name="deductible" placeholder={"Deductible"} />
-                </div>
-                <div>
-                    <br/>
-                    <h2>Add General Liability</h2>
-    
-                    <input type={"text"} name="policyNumber" placeholder={"Policy Number"} />
-                    <input type={"text"} name="startDate" placeholder={"Effective Date"} />
-                    <input type={"text"} name="endDate" placeholder={"Expiration Date"} />
-                    <select name="insurerId">
-                        {insurers.map((insurer) => {
-                            return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
-                        })}
-                    </select>
-                </div>
-        </div>
+    <div>
+    <div>
+        <h2>Add Account</h2>
+        <input
+            type="text"
+            name="BusinessLegalName"
+            value={formData.BusinessLegalName}
+            onChange={handleChange}
+            placeholder="Business Legal Name"
+        /><br/>
+        <input
+            type="text"
+            name="BusinessAddressLine1"
+            value={formData.BusinessAddressLine1}
+            onChange={handleChange}
+            placeholder="Line 1"
+        />
+        <input
+            type="text"
+            name="BusinessAddressLine2"
+            value={formData.BusinessAddressLine2}
+            onChange={handleChange}
+            placeholder="Line 2"
+        /><br/>
+        <input
+            type="text"
+            name="BusinessCity"
+            value={formData.BusinessCity}
+            onChange={handleChange}
+            placeholder="Business City"
+        />
+        <input
+            type="text"
+            name="BusinessState"
+            value={formData.BusinessState}
+            onChange={handleChange}
+            placeholder="Business State"
+        />
+        <input
+            type="text"
+            name="BusinessZipCode"
+            value={formData.BusinessZipCode}
+            onChange={handleChange}
+            placeholder="Business Zip"
+        />
+    </div>
+    <div>
+        <br/>
+        <h2>Add Auto Liability</h2>
+        <input
+            type="text"
+            name="autoLiability.policyNumber"
+            value={formData.autoLiability.policyNumber}
+            onChange={handleChange}
+            placeholder="Policy Number"
+        />
+        <input
+            type="text"
+            name="autoLiability.startDate"
+            value={formData.autoLiability.startDate}
+            onChange={handleChange}
+            placeholder="Effective Date"
+        />
+        <input
+            type="text"
+            name="autoLiability.endDate"
+            value={formData.autoLiability.endDate}
+            onChange={handleChange}
+            placeholder="Expiration Date"
+        />
+        <select name="autoLiability.insurerId" onChange={handleChange}>
+            {insurers.map((insurer) => {
+                return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
+            })}
+        </select>
+        <input
+            type="text"
+            name="autoLiability.coverageData.combinedSingleLimit"
+            value={formData.autoLiability.coverageData.combinedSingleLimit}
+            onChange={handleChange}
+            placeholder="Combined Single Limit"
+        />
+    </div>
+    <div>
+        <br/>
+        <h2>Add Cargo</h2>
+        <input
+            type="text"
+            name="cargo.policyNumber"
+            value={formData.cargo.policyNumber}
+            onChange={handleChange}
+            placeholder="Policy Number"
+        />
+        <input
+            type="text"
+            name="cargo.startDate"
+            value={formData.cargo.startDate}
+            onChange={handleChange}
+            placeholder="Effective Date"
+        />
+        <input
+            type="text"
+            name="cargo.endDate"
+            value={formData.cargo.endDate}
+            onChange={handleChange}
+            placeholder="Expiration Date"
+        />
+        <select name="cargo.insurerId" onChange={handleChange}>
+            {insurers.map((insurer) => {
+                return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
+            })}
+        </select>
+        <input
+            type="text"
+            name="cargo.coverageData.limit"
+            value={formData.cargo.coverageData.limit}
+            onChange={handleChange}
+            placeholder="Cargo Limit"
+        />
+        <input
+            type="text"
+            name="cargo.coverageData.deductible"
+            value={formData.cargo.coverageData.deductible}
+            onChange={handleChange}
+            placeholder="Cargo Deductible"
+        />
+        <input
+            type="text"
+            name="cargo.coverageData.trailerInterchangeLimit"
+            value={formData.cargo.coverageData.trailerInterchangeLimit}
+            onChange={handleChange}
+            placeholder="Trailer Interchange Limit"
+        />
+        <input
+            type="text"
+            name="cargo.coverageData.reeferDeductible"
+            value={formData.cargo.coverageData.reeferDeductible}
+            onChange={handleChange}
+            placeholder="Reefer Deductible"
+        />
+    </div>
+    <div>
+        <br/>
+        <h2>Add Physical Damage</h2>
+        <input
+            type="text"
+            name="physicalDamage.policyNumber"
+            value={formData.physicalDamage.policyNumber}
+            onChange={handleChange}
+            placeholder="Policy Number"
+        />
+        <input
+            type="text"
+            name="physicalDamage.startDate"
+            value={formData.physicalDamage.startDate}
+            onChange={handleChange}
+            placeholder="Effective Date"
+        />
+        <input
+            type="text"
+            name="physicalDamage.endDate"
+            value={formData.physicalDamage.endDate}
+            onChange={handleChange}
+            placeholder="Expiration Date"
+        />
+        <select name="physicalDamage.insurerId" onChange={handleChange}>
+            {insurers.map((insurer) => {
+                return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
+            })}
+        </select>
+        <input
+            type="text"
+            name="physicalDamage.coverageData.deductible"
+            value={formData.physicalDamage.coverageData.deductible}
+            onChange={handleChange}
+            placeholder="Deductible"
+        />
+    </div>
+    <div>
+        <br/>
+        <h2>Add General Liability</h2>
+        <input
+            type="text"
+            name="generalLiability.policyNumber"
+            value={formData.generalLiability.policyNumber}
+            onChange={handleChange}
+            placeholder="Policy Number"
+        />
+        <input
+            type="text"
+            name="generalLiability.startDate"
+            value={formData.generalLiability.startDate}
+            onChange={handleChange}
+            placeholder="Effective Date"
+        />
+        <input
+            type="text"
+            name="generalLiability.endDate"
+            value={formData.generalLiability.endDate}
+            onChange={handleChange}
+            placeholder="Expiration Date"
+        />
+            <input
+        type="text"
+        name="generalLiability.coverageData.limits.eachOccurrence"
+        value={formData.generalLiability.coverageData.limits.eachOccurrence}
+        onChange={handleChange}
+        placeholder="Each Occurrence"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.limits.damageToRentedPremises"
+        value={formData.generalLiability.coverageData.limits.damageToRentedPremises}
+        onChange={handleChange}
+        placeholder="Damage to Rented Premises"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.limits.medicalExpense"
+        value={formData.generalLiability.coverageData.limits.medicalExpense}
+        onChange={handleChange}
+        placeholder="Medical Expense"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.limits.personalAndAdvertisingInjury"
+        value={formData.generalLiability.coverageData.limits.personalAndAdvertisingInjury}
+        onChange={handleChange}
+        placeholder="Personal and Advertising Injury"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.limits.generalAggregate"
+        value={formData.generalLiability.coverageData.limits.generalAggregate}
+        onChange={handleChange}
+        placeholder="General Aggregate"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.limits.productsCompletedOperationsAggregate"
+        value={formData.generalLiability.coverageData.limits.productsCompletedOperationsAggregate}
+        onChange={handleChange}
+        placeholder="Products Completed Operations Aggregate"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.occur"
+        value={formData.generalLiability.coverageData.occur}
+        onChange={handleChange}
+        placeholder="Occurrence"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.claimsMade"
+        value={formData.generalLiability.coverageData.claimsMade}
+        onChange={handleChange}
+        placeholder="Claims Made"
+    />
+    <input
+        type="text"
+        name="generalLiability.coverageData.generalAggregateAppliesPerPolicy"
+        value={formData.generalLiability.coverageData.generalAggregateAppliesPerPolicy}
+        onChange={handleChange}
+        placeholder="General Aggregate Applies Per Policy"
+    />
+        <select name="generalLiability.insurerId" onChange={handleChange}>
+            {insurers.map((insurer) => {
+                return <option key={insurer.id} value={insurer.id}>{insurer.name}</option>;
+            })}
+        </select>
+    </div>
+</div>
         <div>
             <button type="submit">Submit</button>
         </div>
